45f2215067d7386f7c75b5064f46f558d00b5d44
Lennart Kowitz <Lennart.Kowitz@isas.de>
2022-01-26 12:46:53 +0100

    Commented out the imports as well on dockwidget test

--- src/vessel_express/_tests/test_dock_widget.py
+++ src/vessel_express/_tests/test_dock_widget.py
@@ -1,6 +1,7 @@
+"""
 from vessel_express import ExampleQWidget, example_magic_widget
 import numpy as np
-"""
+
 # make_napari_viewer is a pytest fixture that returns a napari viewer object
 # capsys is a pytest fixture that captures stdout and stderr output streams
 def test_example_q_widget(make_napari_viewer, capsys):
