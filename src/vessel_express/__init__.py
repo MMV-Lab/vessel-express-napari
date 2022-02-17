99c7564e5a5fa89dda56508d987f8e51dded1dfc
Lennart Kowitz <Lennart.Kowitz@isas.de>
2022-02-17 12:39:41 +0100

    Now exporting both plugins

--- src/vessel_express/__init__.py
+++ src/vessel_express/__init__.py
@@ -4,5 +4,5 @@ __version__ = "0.0.1"
 
 from ._reader import napari_get_reader
 
-from ._dock_widget import napari_experimental_provide_dock_widget, ParameterTuning
+from ._dock_widget import napari_experimental_provide_dock_widget, ParameterTuning, Evaluation
 
