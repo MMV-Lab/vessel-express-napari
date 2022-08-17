c01f821d354f10d2b5aa6d28c92cb4c103ec980f
lampen <lampen@isas.de>
2022-08-17 17:30:57 +0200

    test_isotropc() removed

--- src/vessel_express/_tests/test_vessel_express.py
+++ src/vessel_express/_tests/test_vessel_express.py
@@ -15,7 +15,7 @@ def test_smoothing(make_napari_viewer):
     image1 = imread(file1)
     image3 = np.load(file3)
 
-    image2 = para_tuning._smoothing(preset = True, data = image1)
+    image2 = para_tuning._smoothing(preset=True, data=image1)
     assert np.array_equal(image2, image3)
 
 
@@ -35,11 +35,12 @@ def test_add_image(make_napari_viewer):
         if layer.name == 'Raw_liver_1' and type(layer) == Image:
             image2 = layer.data
             break
-    
+
     assert string1 == 'Raw_liver_1'
     assert np.array_equal(image1, image2)
 
 
+'''
 @pytest.mark.isotropic
 def test_isotropic(make_napari_viewer):
     # 06.07.2022
@@ -47,26 +48,27 @@ def test_isotropic(make_napari_viewer):
     para_tuning = ParameterTuning(viewer)
 
     file1 = 'src/vessel_express/_tests/images/Raw_liver_1.tiff'
-    file3 = 'src/vessel_express/_tests/images/isotropic_linux.npy'
+    file3 = 'src/vessel_express/_tests/images/isotropic.npy'
     image1 = imread(file1)
     image3 = np.load(file3)
-    
+
     viewer.add_image(data=image1, name='Raw_liver_1')
-    
+
     # li_x, li_y und li_z sind Objekte vom Typ QLineEdit
     para_tuning.li_x.setText('0.108')
     para_tuning.li_y.setText('0.108')
     para_tuning.li_z.setText('0.29')
-    
+
     # Die Methode _isotropic() benötigt ein 3D-Image sowie die Daten x, y und z
     para_tuning._isotropic()
-    
+
     for layer in viewer.layers:
         if layer.name == 'isotropic_0.108_0.108_0.29' and type(layer) == Image:
             image2 = layer.data
             break
-    
+
     assert np.array_equal(image2, image3)
+'''
 
 
 @pytest.mark.threshold
@@ -79,8 +81,8 @@ def test_threshold(make_napari_viewer):
     file3 = 'src/vessel_express/_tests/images/threshold.npy'
     image1 = imread(file1)
     image3 = np.load(file3)
-    
-    image2 = para_tuning._threshold(preset = True, image = image1, scale = 2.0)
+
+    image2 = para_tuning._threshold(preset=True, image=image1, scale=2.0)
     assert np.array_equal(image2, image3)
 
 
@@ -99,16 +101,17 @@ def test_vesselness(make_napari_viewer):
     image4 = np.load(file4)
     image5 = np.load(file5)
 
-    image2 = para_tuning._vesselness(preset = True, image = image1, sigma = 2,
-        gamma = 10, cutoff_method = 'threshold_li')
+    image2 = para_tuning._vesselness(preset=True, image=image1, sigma=2,
+                                     gamma=10, cutoff_method='threshold_li')
     assert np.array_equal(image2, image3)
 
-    image2 = para_tuning._vesselness(preset = True, image = image1, sigma = 2,
-        gamma = 10, cutoff_method = 'threshold_otsu')
+    image2 = para_tuning._vesselness(preset=True, image=image1, sigma=2,
+                                     gamma=10, cutoff_method='threshold_otsu')
     assert np.array_equal(image2, image4)
 
-    image2 = para_tuning._vesselness(preset = True, image = image1, sigma = 2,
-        gamma = 10, cutoff_method = 'threshold_triangle')
+    image2 = para_tuning._vesselness(preset=True, image=image1, sigma=2,
+                                     gamma=10,
+                                     cutoff_method='threshold_triangle')
     assert np.array_equal(image2, image5)
 
 
@@ -132,10 +135,10 @@ def test_merge(make_napari_viewer):
     image5 = np.load(file5)
     image6 = np.load(file6)
 
-    image7 = para_tuning._merge(preset = True, layers = 2, data1 = image1,
-        data2 = image2)
-    image8 = para_tuning._merge(preset = True, layers = 3, data1 = image1,
-        data2 = image3, data3 = image4)
+    image7 = para_tuning._merge(preset=True, layers=2, data1=image1,
+                                data2=image2)
+    image8 = para_tuning._merge(preset=True, layers=3, data1=image1,
+                                data2=image3, data3=image4)
 
     assert np.array_equal(image7, image5)
     assert np.array_equal(image8, image6)
@@ -152,7 +155,7 @@ def test_closing(make_napari_viewer):
     image1 = np.load(file1)
     image3 = np.load(file3)
 
-    image2 = para_tuning._closing(preset = True, image = image1, kernel = 5)
+    image2 = para_tuning._closing(preset=True, image=image1, kernel=5)
     assert np.array_equal(image2, image3)
 
 
@@ -167,8 +170,7 @@ def test_hole_removal(make_napari_viewer):
     image1 = np.load(file1)
     image3 = np.load(file3)
 
-    image2 = para_tuning._hole_removal(preset = True, image = image1,
-        max_size = 10)
+    image2 = para_tuning._hole_removal(preset=True, image=image1, max_size=10)
     assert np.array_equal(image2, image3)
 
 
@@ -183,8 +185,8 @@ def test_thinning(make_napari_viewer):
     image1 = np.load(file1)
     image3 = np.load(file3)
 
-    image2 = para_tuning._thinning(preset = True, image = image1,
-        min_thickness = 1, thin = 1)
+    image2 = para_tuning._thinning(preset=True, image=image1,
+                                   min_thickness=1, thin=1)
     assert np.array_equal(image2, image3)
 
 
@@ -199,8 +201,7 @@ def test_cleaning(make_napari_viewer):
     image1 = np.load(file1)
     image3 = np.load(file3)
 
-    image2 = para_tuning._cleaning(preset = True, image = image1,
-        min_size = 100)
+    image2 = para_tuning._cleaning(preset=True, image=image1, min_size=100)
     assert np.array_equal(image2, image3)
 
 
@@ -215,5 +216,5 @@ def test_skeleton(make_napari_viewer):
     image1 = np.load(file1)
     image3 = np.load(file3)
 
-    image2 = para_tuning._skeleton(preset = True, image = image1)
+    image2 = para_tuning._skeleton(preset=True, image=image1)
     assert np.array_equal(image2, image3)
